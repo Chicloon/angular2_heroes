f625ba4d93e1e2a86ad0c3fec536bc1081624813
Chicloon <Chicloon@gmail.com>
2016-08-08 17:02:18 +0900

    made routing stuff

---
+++ app/heroes.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero.ts';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: 'app/heroes.component.html',
+  styleUrls: ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
+
+})
+
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  title = 'Tour of Heroes';
+  selectedHero : Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private router: Router) {
+  }
+  
+  getHeroes() {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(){
+    this.getHeroes();
+  }
+  
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail() {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}
+
